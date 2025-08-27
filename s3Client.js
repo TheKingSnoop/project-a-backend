76cdeeb79c88c992a6bd0bfef703bb891bda6d68
Israel <93683580+blueishegg@users.noreply.github.com>
2025-08-27 16:39:55 +0100

    first attempt at storing PDFs to AWS bucket - code needs reformatting

--- s3Client.js
+++ s3Client.js
@@ -8,7 +8,7 @@ const { config, S3 } = AWS;
 config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "eu-west-2",
+  region: process.env.AWS_REGION || "eu-west-2",
 });
 
 const s3 = new S3();
