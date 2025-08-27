7a9825324a286b7f6efaa40efdfa041657657b69
Marcy Calderon <marcy.calderon.patino@opencastsoftware.com>
2025-08-27 15:37:23 +0100

    started configuring puppeteer to generate invoice pdf

--- Functions/invoices.js
+++ Functions/invoices.js
@@ -1,55 +1,79 @@
+import puppeteer from "puppeteer";
+import { InvoiceTemplate } from "../Invoice_Templates/invoice.js";
+
 export const GetInvoices = async () => {
-  try {
-    // below is just an example for now
-    const invoices = [
-      {
-        id: 1,
-        amount: 100.0,
-        date: "01-01-2025",
-      },
-      {
-        id: 2,
-        amount: 200.0,
-        date: "01-02-2025",
-      },
-    ];
-    return {
-      success: true,
-      payload: invoices,
-    };
-  } catch (error) {
-    return {
-      success: false,
-      message: error.message,
-    };
-  }
+    try {
+        // below is just an example for now
+        const invoices = [
+            {
+                id: 1,
+                amount: 100.0,
+                date: "01-01-2025",
+            },
+            {
+                id: 2,
+                amount: 200.0,
+                date: "01-02-2025",
+            },
+        ];
+        return {
+            success: true,
+            payload: invoices,
+        };
+    } catch (error) {
+        return {
+            success: false,
+            message: error.message,
+        };
+    }
 };
 
 export const GetInvoiceById = async (id) => {
-  try {
-    const invoice = [
-      {
-        id: id,
-        amount: 100.0,
-        date: "01-01-2025",
-      },
-    ];
-    return {
-      success: true,
-      payload: invoice,
-    };
-  } catch (error) {
-    return { success: false, message: error.message };
-  }
+    try {
+        const invoice = [
+            {
+                id: id,
+                amount: 100.0,
+                date: "01-01-2025",
+            },
+        ];
+        return {
+            success: true,
+            payload: invoice,
+        };
+    } catch (error) {
+        return { success: false, message: error.message };
+    }
 };
 
 export const GenerateInvoice = async (invoiceData) => {
-  try {
-    // Logic to generate invoice
-    console.log("Generating invoice with data:", invoiceData);
-    
-    return { success: true, message: "Invoice generated successfully", payload: invoiceData };
-  } catch (error) {
-    return { success: false, message: error.message };
-  }
+
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        const invoiceHtmlTemplate = InvoiceTemplate(invoiceData)
+
+        await page.setContent(invoiceHtmlTemplate);
+        const pdfBuffer = await page.pdf({ format: 'A4' });
+
+        await browser.close();
+        return pdfBuffer;
+
+        // return { success: true, message: "Invoice generated successfully", payload: invoiceData };
+    } catch (error) {
+        return { success: false, message: error.message };
+    }
 };
+
+
+// const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+
+
+// await page.setContent(html);
+//     const pdfBuffer = await page.pdf({ format: 'A4' });
+
+//     await browser.close();
+//     return pdfBuffer;
