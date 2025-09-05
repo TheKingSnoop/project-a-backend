98407a2397ab44827b34e50e0d1138758edabb9e
Marcy Calderon <marcy.calderon.patino@opencastsoftware.com>
2025-09-05 09:54:54 +0100

    updated the html invoice template

--- Invoice_Templates/invoice.js
+++ Invoice_Templates/invoice.js
@@ -1,21 +1,72 @@
 export function InvoiceTemplate(invoiceData) {
     const html = `
         <html>
-        <head>
-            <style>
-                body { font-family: Arial, sans-serif; margin: 20px; }
-                h1 { color: #333; }
-                .invoice-box { border: 1px solid #ddd; padding: 20px; }
+            <head>
+                <style>
+                    body { font-family: Arial, sans-serif; margin: 40px; }
+                    h1 { color: #333; }
+                    thead tr { background-color: #f2f2f2; }
+                    .title { text-align: center; }
+                    .billing-info { display: flex; justify-content: space-between; }
+                    .invoice-box, .invoice-total { text-align: right; }
+                    .blue-text { color: #005ac2; }
                 </style>
-        </head>
-        <body>
-            <div class="invoice-box">
-                <h1>Invoice</h1>
-                <p>Customer: ${invoiceData.clientName}</p>
-                <p>Amount: $${invoiceData.invoiceItems[0].amount}</p>
-                <p>Due Date: $${invoiceData.dueDate}</p>
-            </div>
-        </body>
+            </head>
+            <body>
+                <h1 class="title blue-text">INVOICE</h1>
+                <div class="your-company">
+                    <p class="blue-text"><strong>${invoiceData.nameOfYourCompany ? invoiceData.nameOfYourCompany : ""}</strong></p>
+                    <p>${invoiceData.yourAddress}</p>
+                    <p>${invoiceData.yourCity}, ${invoiceData.yourPostCode}</p>
+                    <p>${invoiceData.yourPhoneNumber}</p>
+                    <p>${invoiceData.yourEmail}</p>
+                </div>
+                <div class="billing-info">
+                    <div class="bill-to">
+                        <h3 class="blue-text">Bill To:</h3>
+                        <p>${invoiceData.companyName}</p>
+                        <p>${invoiceData.clientName}</p>
+                        <p>${invoiceData.clientAddress}</p>
+                        <p>${invoiceData.clientCity}, ${invoiceData.clientPostCode}</p>
+                        <p>${invoiceData.clientEmail}</p>
+                    </div>
+                    <div class="invoice-box">
+                        <h3 class="blue-text">Invoice Number: ${invoiceData.referenceNumber}</h3>
+                        <p>Invoice Date: ${invoiceData.issueDate}</p>
+                        <p>Due Date: ${invoiceData.dueDate}</p>
+                    </div>
+                </div>
+                <table border="1" cellpadding="10" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th>DESCRIPTION</th>
+                            <th>QTY</th>
+                            <th>UNIT COST</th>
+                            <th>TOTAL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${invoiceData.invoiceItems.map(item => `<tr>
+                                <td style='width:70%'>${item.description}</td>
+                                <td>${item.quantity}</td>
+                                <td>£${item.price}</td>
+                                <td>£${item.amount}</td>
+                            </tr>`).join('')}
+                    </tbody>
+                </table>
+                <div class="invoice-total">
+                    <h3 class="blue-text">Invoice Total:</h3>
+                    <p>Subtotal: £${invoiceData['invoiceItems-total'].amount}</p>
+                    <p>VAT (${invoiceData.vatPercentage}%): £${invoiceData.vat}</p>
+                    <h4 style="border-top: 1px solid #005ac2; display: inline; border-bottom: 1px solid #005ac2; background-color: #d8e2ff;"><strong>Total: £${invoiceData.finalTotal}</strong></h4>
+                </div>
+                <div class="bank-details">
+                    <h3 class="blue-text">Bank Details:</h3>
+                    <p>Sort Code: ${invoiceData.sortCode}</p>
+                    <p>Account Number: ${invoiceData.accountNumber}</p>
+                    <p>Bank Name: ${invoiceData.bankName}</p>
+                </div>
+            </body>
         </html>
         `;
     return html;
