ce7a4d8b9e4827d1ff4fdf2303a90ef88ac43e40
Israel <93683580+blueishegg@users.noreply.github.com>
2025-10-07 19:20:31 +0100

    removed console logs

--- middleware/check_auth.js
+++ middleware/check_auth.js
@@ -3,10 +3,7 @@ import jwt from "jsonwebtoken";
 const checkAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    // console.log(token, "token in middleware");
-    console.log(process.env.JWT_SECRET, "jwt key in middleware");
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decodedToken, "decoded token in middleware");
     req.userData = { id: decodedToken.id, name: decodedToken.name };
     next();
   } catch (error) {
